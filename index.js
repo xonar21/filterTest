f73dcb750ce9790e77a92d4a97b14b7e8fb05d7c
Spynu Jean <jspynu14@mail.ru>
2022-05-31 15:09:49 +0400

    test

---
+++ index.js
@@ -0,0 +1,22 @@
+let courses = [
+    { name: "Courses in England", prices: [0, 100] }, 
+    { name: "Courses in Germany", prices: [500, null] }, 
+    { name: "Courses in Italy", prices: [100, 200] }, 
+    { name: "Courses in Russia", prices: [null, 400] },
+    { name: "Courses in China", prices: [50, 250] },
+    { name: "Courses in USA", prices: [200, null] },
+    { name: "Courses in Kazakhstan", prices: [56, 324] },
+    { name: "Courses in France", prices: [null, null] },
+];
+
+// Варианты цен (фильтры), которые ищет пользователь
+let requiredRange1 = [null, 200];
+let requiredRange2 = [100, 350];
+let requiredRange3 = [200, null];
+
+
+function filtered (range) {
+    const res = courses.filter(item => (item.prices[0] >= range[0] && item.prices[0] <= range[1]) && (item.prices[1] <= range[1] ))
+    console.log(res)
+}
+filtered(requiredRange1);
